f1f33e2696bc66a0d0eea079962952f2609df2e4
pedro <pitthdez7@gmail.com>
2019-05-17 15:06:30 -0500

    modificacion de js

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ console.log('Hola mundo');
 git init  // crear el archivo de git
 git status //verificar el estatus del archivo
 git add nombredelarchivo // agregar archivo a comitte
-git config --global user.email ""
+git config --global user.email "" //email de la persona
+git config --global user.name "" //mombre del usuario
+git commit // para subir cambios
+:wq! //para termiar el commit
+git log // has para ver quien hizo los cambios en el archivo o cmmit
