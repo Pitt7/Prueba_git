abeac17796e9003ca8777f81d9727b6d9c33f295
pedro <pitthdez7@gmail.com>
2019-05-17 15:25:12 -0500

    Prueba 1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ git add nombredelarchivo // agregar archivo a comitte
 git config --global user.email "" //email de la persona
 git config --global user.name "" //mombre del usuario
 git commit // para subir cambios
-git -m "comentario" // para hacer commit sin necesidad de que me abra el apartado de escribir mensaje y salir de la linea
+git commit -m "comentario" // para hacer commit sin necesidad de que me abra el apartado de escribir mensaje y salir de la linea
 :wq! //para termiar el commit
 git log // has para ver quien hizo los cambios en el archivo o cmmit
 git checkout -- nombredelarchivo // para descartar cambios
