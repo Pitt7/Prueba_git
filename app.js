955476c1186225052417699054d8b711ea86949d
pedro <pitthdez7@gmail.com>
2019-05-17 14:56:54 -0500

    mi primer commi

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('Hola mundo');
+
+
+git init  // crear el archivo de git
+git status //verificar el estatus del archivo
+git add nombredelarchivo // agregar archivo a comitte
+git config --global user.email ""
