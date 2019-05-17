3bc8ce2eb97a897fd1d0ebf1fc8a662ecc295fce
pedro <pitthdez7@gmail.com>
2019-05-17 16:45:14 -0500

    Prueba

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log('Hola mundo');
 git init  // crear el archivo de git
 git status //verificar el estatus del archivo
 git add nombredelarchivo // agregar archivo a comitte
+git add . // agregar todos los archivos con cambio en una sola linea
 git config --global user.email "" //email de la persona
 git config --global user.name "" //mombre del usuario
 git commit // para subir cambios
@@ -13,3 +14,6 @@ git log // has para ver quien hizo los cambios en el archivo o cmmit
 git checkout -- nombredelarchivo // para descartar cambios
 git diff // para verificar la diferencia 
 .gitignore // archivo para meter las carpetas que quiero que ignora y no me muestre que tiene cambios
+git branch //para verificar 
+git branch nombredeversion // agregar una version diferente a master
+git checkout nombredeversion // para cambiar de version
